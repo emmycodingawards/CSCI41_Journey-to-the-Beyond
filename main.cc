88c7dd1c1e03938bea23652bd7ded0fdb2b8b4ec
Emmy Issa <38083298+emmycodingawards@users.noreply.github.com>
2018-04-10 16:28:15 -0700

    Creating main.cc
    Okay so this is going to be our main file that everyone's code is going to eventually go into. I think we should try the different branch system for our three main stuffs (Preet, Denise, and mine) but we'll see. 
    We shouldn't need any header files since we're going to use the STL heap and hash table.

---
+++ main.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <queue> //HASH TABLE STL: priority_queue
+#include <unordered_map> //HASH TABLE
+#include <algorithm> //make_heap, pop_heap, push_heap, sort_heap
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
