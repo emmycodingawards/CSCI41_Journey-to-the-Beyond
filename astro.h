9b1afd7933c5d8267e1dc7ba3a00445575c256cf
Emmy Issa <38083298+emmycodingawards@users.noreply.github.com>
2018-04-11 11:18:39 -0700

    Creation of Astro class so far
    These are my ideas, I'll add more later

--- astro.h
+++ astro.h
@@ -1,1 +1,19 @@
+#pragma once
+#include <iostream>
+using namespace std;
 
+//Astronaut constructor
+//Initializes every astronaut
+//Can only be 4 astronauts in rocket at a time
+
+struct Astro {
+  string name;
+  string position; //Four positions: Commander, Pilot, Mission Specialist, Flight Engineer
+  int ID = 0; //Will be unique 4-digit ID
+  int health = 100; //When health reaches 0, astronaut dies
+  int time_on_rocket = 0; //in months, max 6 months and astronaut has to return to Earth
+ 
+  bool operator< (const Astro &rhs) const {
+        return ID > rhs.ID;
+  }
+};
