ef2e6d8ceec1bc9f89ad3a81a23797b8373377cd
maplemaplemaple <38085675+maplemaplemaple@users.noreply.github.com>
2018-04-11 13:48:25 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -10,6 +10,19 @@
 #include "astro.h" 
 
 using namespace std;
+//i want the user to input days!
+int astrotime ( int days) {
+ int convert;
+ if ( days > 182.5) {
+ return error;
+   else if ( 
+ } 
+return days_n_months;
+}
+
+
+
+
 
 
 
