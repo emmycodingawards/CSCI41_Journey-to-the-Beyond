fcd65d3b539fc4b4f210844748da562de23ccb72
Emmy Issa <38083298+emmycodingawards@users.noreply.github.com>
2018-04-12 13:22:48 -0700

    Update astro.h

--- astro.h
+++ astro.h
@@ -9,9 +9,9 @@ using namespace std;
 struct Astro {
   string name;
   string position; //Four positions: Commander, Pilot, Mission Specialist, Flight Engineer
-  int ID = 0; //Will be unique 4-digit ID
+  int ID = 0; //Will be unique 4-digit ID - key for unordered map
   int health = 100; //When health reaches 0, astronaut dies
-  int time_on_rocket = 0; //in months, max 6 months and astronaut has to return to Earth
+  int days_on_rocket = 0; //in months, max 183 days (6 months) and astronaut has to return to Earth
  
   bool operator< (const Astro &rhs) const {
         return ID > rhs.ID;
