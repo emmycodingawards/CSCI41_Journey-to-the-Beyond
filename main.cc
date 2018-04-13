814417cf1114fc4e3170a3857ae4f53e0ad5ff87
maplemaplemaple <38085675+maplemaplemaple@users.noreply.github.com>
2018-04-12 17:02:45 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -11,10 +11,20 @@
 using namespace std;
 
 //CALCULATE ESCAPE SPEED
-float escape_velocity () {
- double g =  
+double escape_velocity ( ) {
+ const double G = 6.673*pow(10,-11);
+ const double r = 6.38*pow(10,6);
+ const double Mearth = 5.98*pow(10,-11);
+ double escapevelocity = 2*G*Mearth/r;
+ escapevelocity= sqrt(escapevelocity);
 }
-
+//Mass1=is the mass of the planet Mass2=rocketmass rad=radius
+//General Function that calculates each gravational poll of each planet..
+double gravational_pull( double Mass1, double Mass2, double rad){
+ const double G= 6.672*pow(10,-11);
+ gravitstional_force = G*;
+ 
+}
 int main() {
 
 
