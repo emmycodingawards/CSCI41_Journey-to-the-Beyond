372257b5df38736879ccfd3f8912a38cabbd1da3
Emmy Issa <38083298+emmycodingawards@users.noreply.github.com>
2018-04-13 11:17:47 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -8,24 +8,34 @@
 #include <cstdlib>
 #include <cctype>
 #include "astro.h" 
+#include "planet.h"
 using namespace std;
-// DONE BY DENISE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
-//CALCULATE ESCAPE SPEED
-double escape_velocity ( ) {
- const double G = 6.673*pow(10,-11);
- const double r = 6.38*pow(10,6);
- const double Mearth = 5.98*pow(10,-11);
- double escapevelocity = 2*G*Mearth/r;
- escapevelocity= sqrt(escapevelocity);
- return escapevelocity;
+
+//Calculates escape velocity for each planet
+double escape_velocity (string planet) {
+const double G = 6.674*pow(10,-11); //universal gravitational constant for every planet
+ if (planet == "Mercury") {
+ }
+ if (planet == "Venus") {
+ }
+ if (planet == "Earth") {
+  const double r = 6.38*pow(10,6); //radius in meters
+  const double M = 5.98*pow(10,-11); //mass in kilograms
+ }
+ return double v = sqrt(2*G*M/r); //in m/s
 }
-//Mass1=is the mass of the planet Mass2=rocketmass rad=radius distance between the planet and rocket
-//General Function that calculates each gravational poll of each planet..
-double gravitational_pull( double Mass1, double Mass2, double rad){
- const double G= 6.672*pow(10,-11);
- gravitational_force = G*Mass1*Mass2/pow(rad,2);
- return gravitational_force;
+
+//Calculates gravitational pull for each planet
+double gravitational_pull(string planet){
+ const double G = 6.674*pow(10,-11); //universal gravitational constant for every planet
+ const double m_rocket = 2,800,000 //mass of rocket in kilograms
+ double r =     //distance from rocket to each planet
+ if (planet == "Earth") {
+  const double m_planet = 
+ }
+ return double F = G*m_rocket*m_planet/pow(r,2); //force of gravity in Newtons
 }
+
 //acceleration v1= previous escape_velocity v2=current escape velocity
 double acceleration_formula ( double v1, double v2, int t) {
 
@@ -35,7 +45,7 @@ double acceleration_formula ( double v1, double v2, int t) {
 int main() {
  cout << "5,4,3,2,1" << " " << " Welcome to Yenrek" << endl;
  cout << "It is the year 2077. Technology has advanced so that it is possible to travel to all 9 planets ( YES PLUTO IS A PLANET) in a year given tyou chose the right variables." << endl;
- cout << " Please input amount of watter:" << endl; 
+ cout << " Please input amount of water:" << endl; 
  cin >>  "
 
 
