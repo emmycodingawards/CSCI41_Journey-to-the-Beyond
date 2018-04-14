ff303789116811246cd4795f5a4a07b47b61a080
Emmy Issa <38083298+emmycodingawards@users.noreply.github.com>
2018-04-13 20:12:10 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -16,8 +16,7 @@ void die() {
     exit(1);
 }
 
-
-/Calculates escape velocity for each planet
+//Calculates escape velocity for each planet
 double escape_velocity(string planet) {
     double v = 0;                           //escape velocity in m/s
     double mass = 0;                        //mass in kilograms
@@ -57,5 +56,33 @@ double escape_velocity(string planet) {
     return v = sqrt(2 * G * mass / radius);
 }
 
-  
+  //Calculates gravitational pull for each planet
+double gravitational_pull(string planet) {
+    double F = 0;                           //force of gravity in Newtons
+    double m_planet = 0;                    //mass of planet in kg
+    const double G = 6.674 * pow(10, -11); //universal gravitational constant for every planet
+    const double m_rocket = 2800000;      //mass of rocket in kilograms
+    const double r = 36 * pow(10, 6);     //distance from rocket to each planet - it's going to be constant for us
+    if (planet == "Mercury") {
+        m_planet = 3.30 * pow(10, 23);
+    } else if (planet == "Venus"){
+        m_planet = 4.87 * pow(10, 24);
+    } else if (planet == "Earth") {
+         m_planet = 5.98 * pow(10, -11);
+    } else if (planet == "Mars") {
+         m_planet = 6.42 * pow(10, 23);
+    } else if (planet == "Jupiter") {
+         m_planet = 1.90 * pow(10, 27);
+    } else if (planet == "Saturn") {
+         m_planet = 5.68 * pow(10, 26);
+    } else if (planet == "Uranus") {
+         m_planet = 8.68 * pow(10, 25);
+    } else if (planet == "Neptune") {
+         m_planet = 1.02 * pow(10, 26);
+    } else if (planet == "Pluto") {
+         m_planet = 1.27 * pow(10, 22);
+    } else die();
+    return  F = G * m_rocket * m_planet / pow(r, 2);
+}
+
  
