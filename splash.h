baf8684d68d2081ffd6e1d545e4bc7623184a504
Preet-Jyotpreet <38120430+Preet-Jyotpreet@users.noreply.github.com>
2018-04-14 16:35:58 -0700

    Update splash.h

--- splash.h
+++ splash.h
@@ -194,5 +194,35 @@ void splash4() {
         wait_ticks(0.75 * CLOCKS_PER_SEC);
         clear();
 }
+void words5(int frame) {
+    int x = 60-frame, y = 0, line = 0;
+
+    mvprintw(y+line++,x,"         __            ");
+    mvprintw(y+line++,x,"        / /_           ");
+    mvprintw(y+line++,x,"       | '_ \\        ");
+    mvprintw(y+line++,x,"       | (_) |   _   _   _   _   _   _   _   _   _                ");
+    mvprintw(y+line++,x,"        \\___/   (_) (_) (_) (_) (_) (_) (_) (_) (_)       ");
+
+}
+
+void splash5() {
+    int x = 0, y = 0; //Holds offsets to draw the ASCII art
+    for (int i = 0; i < 60; i++) {
+        timeout(10);
+        int ch = getch(); //Allow them to hit a key to skip the splash screen
+        if (ch != ERR) return;
+
+        //Redraw splash screen
+        clear();
+        words5(i);
+        refresh();
+
+        wait_ticks(0.05 * CLOCKS_PER_SEC);
+    }
+    wait_ticks(0.75 * CLOCKS_PER_SEC);
+    clear();
+}
+
+
 
 
